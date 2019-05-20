57786031c7adebfdee1ff048316e1c098bd68cfc
Neverson Bento da Silva <neversonbs13@gmail.com>
2019-05-19 23:06:32 -0300

    commit

---
+++ src/br/com/treinaweb/springmvc/dominios/Musica.java
@@ -0,0 +1,30 @@
+package br.com.treinaweb.springmvc.dominios;
+
+import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Data
+@Entity
+@Table(name = "musicas")
+public class Musica {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false, length = 10)
+    private String nome;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.DATE)
+    @DateTimeFormat(pattern = "dd/MM/yyyy")
+    private Date dataCriacao;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    private Album album;
+
+
+}
